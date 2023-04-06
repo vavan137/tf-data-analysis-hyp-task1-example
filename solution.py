660f34a494defa675fac754b034adc306e6073fa
vavan137 <113305894+vavan137@users.noreply.github.com>
2023-04-07 01:56:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5463739632 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +12,10 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p = 0.09
+    cr = norm.ppf(p)
+    t_sh = (x_success+y_success)/(x_cnt+y_cnt)
+    result = False
+    if (y_success/y_cnt - x_success/x_cnt) / np.sqrt( t_sh*(1-t_sh)/x_cnt + t_sh*(1-t_sh)/y_cnt) < cr:
+      result = True
+    return result # Ваш ответ, True или False
